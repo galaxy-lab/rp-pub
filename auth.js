911915fef66ef25b418ce3b537e7ed7446bd9287
Paulo Cesar Alves <67027558+pcalves1@users.noreply.github.com>
2022-05-23 21:04:19 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -22,3 +22,29 @@ export const azureDevopsApi = axios.create({
         password: process.env.AZURE_DEVOPS_PAT
     }
 })
+
+
+async function getAzureDevopsProjects() {
+    console.log('Getting azure devops projects list...')
+    var allProjects = []
+    var continuationToken = 0
+    try {
+        do {
+            const response = await azureDevopsApi.get(`/_apis/projects?api-version=2.0&continuationToken=${continuationToken}`, {})
+            if (response.status !== 200) {
+                throw new Error(`Unexpected response code ${response.status} | ${response.statusText}`)
+            }
+            const objProjects = response.data.value
+            objProjects.forEach(objProject => {
+                allProjects.push(objProject.name)
+            })
+            continuationToken = response.headers['x-ms-continuationtoken']
+        } while (continuationToken != null)
+        return allProjects
+    } catch (err) {
+        console.log(`It could not be possible retrieve the porjects list from Azure DevOps. [ERROR]: ${err.message}`)
+        throw err
+    }
+}
+
+console.log(await getAzureDevopsProjects())
